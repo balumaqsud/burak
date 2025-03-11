b612edae3a27580a0c0aaa6dc93152eb1b692504
balumaqsud <ollobergan008@mail.ru>
2025-03-11 11:34:36 +0900

    OLL: initial setup

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+console.log("exec")
+
+//examples
+import moment from "moment";
+
+const currentTime = moment().format("YYYY MM DD")
+console.log(currentTime)
+
+const person: string = "oliver";
+const age: number = 24;
