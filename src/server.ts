a688d45ec0c69e5e1551fd68c1131f8be6bc6237
balumaqsud <ollobergan008@mail.ru>
2025-03-11 14:39:17 +0900

    interface object

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,27 @@ const age: number = 24;
 console.log(myName)
 console.log(age)
 
+interface Bank {
+    name: string,
+    started: number,
+    branch: number,
+    employees: number,
+    motto: string[],
+    logo: string,
+    ravenue: number,
+}
+
+const wise: Bank = {
+    name: "wise",
+    started: 1990,
+    branch: 34,
+    employees: 1990,
+    motto: ['our bank'],
+    logo: "https/something",
+    ravenue: 1.8
+}
+
+console.log(`this bank is ${wise.name}, and it originally started in ${wise.started}. Now it has ${wise.branch}, ${wise.employees}, with ravenue of ${wise.ravenue}M`)
 
 
 //architectual patters: MVC, DI, frontendga -> MVP
