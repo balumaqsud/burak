14599e3b9e722b231c5a9497403fda819aa8762f
balumaqsud <ollobergan008@mail.ru>
2025-03-14 01:49:25 +0900

    feat: env variables

--- src/server.ts
+++ src/server.ts
@@ -1,36 +1,9 @@
-//examples
-import moment from "moment";
+import dotenv from "dotenv";
+dotenv.config()
 
-const currTime = moment().format("YYY MM DD")
-console.log(currTime)
+console.log("PORT", process.env.PORT)
 
-const myName: string = "Ollobergan";
-const age: number = 24;
 
-console.log(myName)
-console.log(age)
-
-interface Bank {
-    name: string,
-    started: number,
-    branch: number,
-    employees: number,
-    motto: string[],
-    logo: string,
-    ravenue: number,
-}
-
-const wise: Bank = {
-    name: "wise",
-    started: 1990,
-    branch: 34,
-    employees: 1990,
-    motto: ['our bank'],
-    logo: "https/something",
-    ravenue: 1.8
-}
-
-console.log(`this bank is ${wise.name}, and it originally started in ${wise.started}. Now it has ${wise.branch}, ${wise.employees}, with ravenue of ${wise.ravenue}`)
 
 
 //architectual patters: MVC, DI, frontendga -> MVP
